8c226083b0c1daaa6d5323c5fdd1c88283220b33
hamza <hamzah@paritie.com>
2022-09-14 04:44:13 +0100

    worked on the header section

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        textColor:"#f3f4f5",
+        yellowColor:"#e1ff57",
+      },
+      fontFamily:{
+        'Montserrat':['Montserrat', 'ui-sans-serif'],
+        'Josfine':[ 'Josefin Sans', 'ui-sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
