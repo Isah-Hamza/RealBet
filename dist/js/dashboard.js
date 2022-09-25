f77b0f55b4bdae50e2a390aa80e4b4965448c388
hamza <hamzah@paritie.com>
2022-09-25 16:20:29 +0100

    added a profile page

--- dist/js/dashboard.js
+++ dist/js/dashboard.js
@@ -20,6 +20,32 @@ $(".single-promotion").slick({
 const dropdownToggler = document.querySelector('[data-dropdownToggler]');
 const dropdown = document.querySelector('[data-dropdown]');
 
+const notToggler = document.querySelector('[data-notToggler]');
+const notDropdown = document.querySelector('[data-notDropdown]');
+
+const placeBetBtn = document.querySelector('[data-placeBetBtn]');
+const placeBetContainer = document.querySelector('[data-placeBetContainer]');
+const betSlipBtn = document.querySelector('[data-betSlipBtn]');
+const betSlipContainer = document.querySelector('[data-betSlipContainer]');
+
+betSlipBtn.addEventListener('click', () => {
+    betSlipContainer.classList.remove('hide');
+    placeBetContainer.classList.add('hide');
+    betSlipBtn.classList.add('active-tab')
+    placeBetBtn.classList.remove('active-tab')
+})
+
+placeBetBtn.addEventListener('click', () => {
+    placeBetContainer.classList.remove('hide');
+    betSlipContainer.classList.add('hide');
+    placeBetBtn.classList.add('active-tab')
+    betSlipBtn.classList.remove('active-tab')
+})
+
 dropdownToggler.addEventListener('click', () => {
     dropdown.classList.toggle('hide');
+})
+
+notToggler.addEventListener('click', () => {
+    notDropdown.classList.toggle('hide');
 })
